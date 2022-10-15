8aac8df592e876c9cd6a72370d7e0e0c283bd07e
Inês Soares <inesosoares.6@gmail.com>
2022-10-15 11:52:26 +0100

    Created project

---
+++ src/components/BottomToolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-bottom-navigation app fixed grow v-model="activeItem" color="primary">
+    <v-btn value="top">
+      <span>Top Stories</span>
+      <v-icon>thumb_up</v-icon>
+    </v-btn>
+
+    <v-btn value="code">
+      <span>Code Examples</span>
+      <v-icon>code</v-icon>
+    </v-btn>
+
+    <v-btn value="favorites">
+      <span>Favorites</span>
+      <v-icon>favorite</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld",
+
+  data() {
+    return {
+      activeItem: "top",
+    };
+  },
+});
+</script>
---
+++ src/plugins/vuetify.js
@@ -0,0 +1,11 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import 'vuetify/styles'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
