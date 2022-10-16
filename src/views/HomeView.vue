6611ebc8f7e79ed3747fbf4b813a7034cead22b6
Inês Soares <inesosoares.6@gmail.com>
2022-10-16 12:02:45 +0100

    Home View done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,143 @@
-
 <template>
+  <v-container>
+    <v-card title="WODs Summary">
+      <v-card-text>
+        <v-row class="center-btns">
+          <v-col class="done-todo">
+            <v-avatar size="60" :color="'secondary'"> 10 </v-avatar><br />
+            <v-divider thickness="0px"></v-divider>
+            Done
+          </v-col>
+          <v-col class="done-todo">
+            <v-avatar size="60" :color="'error'"> 0 </v-avatar><br />
+            <v-divider thickness="0px"></v-divider>
+            To Do
+          </v-col>
+        </v-row>
+        <v-divider class="divider" thickness="1px"></v-divider>
+        <v-row class="center-btns">
+          <v-col v-for="(item, index) in items" :key="index">
+            <v-avatar :color="'secondary'"> {{ item.quantity }} </v-avatar
+            ><br />
+            <v-divider thickness="0px"></v-divider>
+            {{ item.type }}
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <v-divider thickness="0px"></v-divider>
+    <v-card>
+      <v-card-title>
+        <v-icon color="secondary">mdi-swap-horizontal</v-icon>
+        WODs
+      </v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col class="d-flex justify-center">
+            <v-btn variant="outlined" append-icon="mdi-arrow-top-right">
+              Send
+            </v-btn>
+          </v-col>
+          <v-col class="d-flex justify-center">
+            <v-btn variant="outlined" prepend-icon="mdi-arrow-bottom-left">
+              Receive
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+    <v-divider thickness="0px"></v-divider>
+    <v-card>
+      <v-card-title>
+        <v-icon class="dumbbell-icon" color="secondary">mdi-dumbbell</v-icon>
+        Random WOD
+      </v-card-title>
+      <v-card-text>
+        <v-row>
+          <v-col>
+            <v-text-field
+              v-model="time"
+              type="number"
+              :rules="timeRules"
+              label="Max time (min)"
+              required
+            ></v-text-field>
+          </v-col>
+          <v-col class="d-flex justify-center">
+            <v-btn
+              class="go-btn"
+              prepend-icon="mdi-weight-lifter"
+              color="secondary"
+            >
+              GO
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card-text>
+    </v-card>
+  </v-container>
+</template>
 
-    <v-container>
-      <h1 class="headline">[[Home]]</h1>
-      Content is coming to this mobile app very soon!
-    </v-container>
-  
-  </template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "AddWorkout",
+
+  data() {
+    return {
+      time: 0,
+      timeRules: [
+        (v) => !!v || "Field is required",
+        (v) => v >= 0 || "Time must be greater than 0",
+      ],
+      items: [
+        {
+          type: "WOD",
+          quantity: 2,
+        },
+        {
+          type: "AFAP",
+          quantity: 3,
+        },
+        {
+          type: "AMRAP",
+          quantity: 3,
+        },
+        {
+          type: "EMOM",
+          quantity: 2,
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style>
+.v-divider {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+.divider {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+
+.go-btn {
+  margin-top: 10px;
+}
+
+.dumbbell-icon {
+  font-size: 25px;
+}
+
+.center-btns {
+  text-align: center;
+}
+
+.done-todo {
+  font-size: 20px;
+  font-weight: bold;
+}
+</style>
