c7d23938d3daceae5890898b33d722a920570a11
Inês Soares <inesosoares.6@gmail.com>
2023-04-26 21:17:51 +0100

    Simplified graph implementation

--- src/components/PersonalView/pop-ups/CardGraphs.vue
+++ src/components/PersonalView/pop-ups/CardGraphs.vue
@@ -1,16 +1,7 @@
 <template>
   <VueApexCharts
-    v-if="input === 'measurement'"
     type="line"
-    :options="chartOptionsWeight"
-    :series="series"
-    width="100%"
-    height="70px"
-  ></VueApexCharts>
-  <VueApexCharts
-    v-else
-    type="line"
-    :options="chartOptions"
+    :options="input === 'measurement' ? chartOptionsTarget : chartOptions"
     :series="series"
     width="100%"
     height="70px"
@@ -33,7 +24,51 @@ const getMinMax = () => {
   return min === max ? [min - 10, max + 10] : [min, max];
 };
 
-const chartOptionsWeight = ref({
+const chartOptions = ref({
+  chart: {
+    id: "records-evolution",
+    group: "sparks",
+    type: "line",
+    sparkline: {
+      enabled: true,
+    },
+    zoom: {
+      enabled: false,
+    },
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: props.color,
+  tooltip: {
+    theme: "dark",
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  grid: {
+    padding: {
+      top: 10,
+      bottom: 10,
+      left: 10,
+      right: 10,
+    },
+  },
+  xaxis: {
+    categories: props.personalValue.date,
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  markers: {
+    size: 6,
+    strokeWidth: 0,
+    hover: {
+      size: 9,
+    },
+  },
+});
+const chartOptionsTarget = ref({
   annotations: {
     yaxis: [
       {
@@ -46,92 +81,7 @@ const chartOptionsWeight = ref({
     min: getMinMax()[0],
     max: getMinMax()[1],
   },
-  chart: {
-    id: "personal-records-evolution",
-    group: "sparks",
-    type: "line",
-    sparkline: {
-      enabled: true,
-    },
-    zoom: {
-      enabled: false,
-    },
-    toolbar: {
-      show: false,
-    },
-  },
-  colors: props.color,
-  tooltip: {
-    theme: "dark",
-  },
-  stroke: {
-    curve: "smooth",
-  },
-  grid: {
-    padding: {
-      top: 10,
-      bottom: 10,
-      left: 10,
-      right: 10,
-    },
-  },
-  xaxis: {
-    categories: props.personalValue.date,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  markers: {
-    size: 6,
-    strokeWidth: 0,
-    hover: {
-      size: 9,
-    },
-  },
-});
-const chartOptions = ref({
-  chart: {
-    id: "personal-records-evolution",
-    group: "sparks",
-    type: "line",
-    sparkline: {
-      enabled: true,
-    },
-    zoom: {
-      enabled: false,
-    },
-    toolbar: {
-      show: false,
-    },
-  },
-  colors: props.color,
-  tooltip: {
-    theme: "dark",
-  },
-  stroke: {
-    curve: "smooth",
-  },
-  grid: {
-    padding: {
-      top: 10,
-      bottom: 10,
-      left: 10,
-      right: 10,
-    },
-  },
-  xaxis: {
-    categories: props.personalValue.date,
-  },
-  dataLabels: {
-    enabled: false,
-  },
-  markers: {
-    size: 6,
-    strokeWidth: 0,
-    hover: {
-      size: 9,
-    },
-  },
+  ...chartOptions.value
 });
 const series = ref([
   {
@@ -143,8 +93,8 @@ const series = ref([
 watch(
   () => [props.color, props.personalValue],
   () => {
-    if (props.personalValue.name === "Weight") {
-      chartOptionsWeight.value.colors = props.color;
+    if (props.input === 'measurement') {
+      chartOptionsTarget.value.colors = props.color;
     } else {
       chartOptions.value.colors = props.color;
     }
